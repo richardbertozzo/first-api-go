4222df807a81618a081bd66fee24ecbfddd080da
Richard Bertozzo <richardjrbertozzo@gmail.com>
2019-02-11 17:39:15 -0200

    sync gets in api(dog, cat and fox)

--- animals/animals.go
+++ animals/animals.go
@@ -1,12 +1,19 @@
 package animals
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+)
+
+const (
+	API_DOG = "https://dog.ceo/api/breeds/image/random"
+	API_CAT = "https://aws.random.cat/meow"
+	API_FOX = "https://randomfox.ca/floof/"
 )
 
 type Animal struct {
@@ -16,32 +23,70 @@ type Animal struct {
 
 func Routes() *chi.Mux {
 	router := chi.NewRouter()
-	router.Get("/", getImagemAnimals)
+	router.Get("/", getAnimals)
 
 	return router
 }
 
-func getImagemAnimals(w http.ResponseWriter, r *http.Request) {
+func getAnimals(w http.ResponseWriter, r *http.Request) {
 	animals := []Animal{
-		{
-			Name: "Dog",
-			Img:  "dasdkoadoa",
-		},
-		{
-			Name: "Cat",
-			Img:  "dasdkoadoa",
-		},
-		{
-			Name: "Fox",
-			Img:  "dasdkoadoa",
-		},
+		*getDog(),
+		*getCat(),
+		*getFox(),
 	}
 
 	render.JSON(w, r, animals) // A chi router helper for serializing and returning json
 }
 
-func get() {
-	response, err := http.Get("https://dog.ceo/api/breeds/image/random")
+func getDog() *Animal {
+	type DogResponse struct {
+		Status string `json:"status"`
+		Link   string `json:"message"`
+	}
+
+	var dogResponse DogResponse
+	response := getResponse(API_DOG)
+	json.Unmarshal(response, &dogResponse)
+
+	return &Animal{
+		Name: "Dog",
+		Img:  dogResponse.Link,
+	}
+}
+
+func getCat() *Animal {
+	type CatResponse struct {
+		Link string `json:"file"`
+	}
+
+	var catResponse CatResponse
+	response := getResponse(API_CAT)
+	json.Unmarshal(response, &catResponse)
+
+	return &Animal{
+		Name: "Cat",
+		Img:  catResponse.Link,
+	}
+}
+
+func getFox() *Animal {
+	type FoxResponse struct {
+		Image string `json:"image"`
+		Link  string `json:"link"`
+	}
+
+	var foxResponse FoxResponse
+	response := getResponse(API_FOX)
+	json.Unmarshal(response, &foxResponse)
+
+	return &Animal{
+		Name: "Fox",
+		Img:  foxResponse.Image,
+	}
+}
+
+func getResponse(url string) []byte {
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error in get:", err)
 	}
@@ -49,8 +94,8 @@ func get() {
 
 	contents, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		fmt.Printf("%s", err)
+		fmt.Println("Error in read body", err)
 	}
 
-	fmt.Printf("%s\n", string(contents))
+	return contents
 }
